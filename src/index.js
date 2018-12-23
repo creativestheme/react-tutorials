1aa2bfc45ad7012ce4b1bede91884da292af69b9
creativestheme <abusayedrussell@gmail.com>
2018-12-23 13:12:59 +0600

    'Components'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from  'react'
 import ReactDOM from 'react-dom'
 
+function Cartoon(props) {
+  return <h1>Hello, {props.name} on {props.show}</h1>;
+}
 
-setInterval(function(){
+function Show(){
+	return <div>
+		<Cartoon name="Pikachu" show="Pokemon" />
+		<Cartoon name="Jasmine" show="Alladin" />
+	</div>
+}
+
 	ReactDOM.render(
-	<h2>Time Now:- {new Date().toLocaleTimeString()}</h2>,
+	<Show/>,
 	document.getElementById('root')
 	);
-},1000)
